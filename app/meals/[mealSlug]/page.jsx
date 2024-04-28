712a4c99a4c49f0bfa8f6bdce37e7b0c958b194a
HensleyFerrari <hensley_ferrari@hotmail.com>
2024-04-28 08:39:09 -0300

    rendering meal details page

--- app/meals/[mealSlug]/page.jsx
+++ app/meals/[mealSlug]/page.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+import Image from "next/image";
 
-function MealDetails() {
-  return <h1>Meal Details</h1>;
+export default function MealDetails({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />')
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill src={meal.image} alt={meal.title} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto: ${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
-
-export default MealDetails;
