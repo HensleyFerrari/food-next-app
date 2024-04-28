5bf8fa4787e6630031043c2d424b3786081401cc
HensleyFerrari <hensley_ferrari@hotmail.com>
2024-04-28 08:55:04 -0300

    added notfound for unique meal

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,8 +1,8 @@
 export default function NotFound() {
   return (
     <main className="not-found">
-        <h1>Not fond</h1>
-        <p>Unfortunately, we could not find the requested page or resource.</p>
+        <h1>Meal not fond</h1>
+        <p>Unfortunately, we could not find the requested page or meal data.</p>
     </main>
   )
 }
