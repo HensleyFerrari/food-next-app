9af27a4d2aedcc4615ab6096217afe9a9ac5d648
HensleyFerrari <hensley_ferrari@hotmail.com>
2024-04-27 22:04:42 -0300

    added Suspense to fetch meals

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,24 +1,37 @@
-import classes from './page.module.css'
-import Link  from 'next/link';
-import MealsGrid from './../components/meals/meals-grid';
-import { getMeals } from '@/lib/meals';
+import classes from "./page.module.css";
+import Link from "next/link";
+import MealsGrid from "./../components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
 
-export default async function MealsPage() {
-    const meals = await getMeals()
-    return (
-        <>
-            <header className={classes.header}>
-                <h1>Delicious meals, created <span className={classes.highlight}>by you</span></h1>
-                <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
-                <p className={classes.cta}>
-                    <Link href='/meals/share'>
-                        Share Your Favorite Recipe
-                    </Link>
-                </p>
-            </header>
-            <main className={classes.main}>
-                <MealsGrid meals={meals} />
-            </main>
-        </>
-    )
+async function Meals() {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense
+          fallback={<div className={classes.loading}>Fetching meals ...</div>}
+        >
+          <Meals />
+        </Suspense>
+      </main>
+    </>
+  );
 }
