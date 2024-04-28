8f27b4faa235e01bbf3e26758733368027273d1e
HensleyFerrari <hensley_ferrari@hotmail.com>
2024-04-28 08:15:02 -0300

    handling error

---
+++ app/meals/error.jsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function MealsError() {
+  return (
+    <div className="error">
+        <h1>An error occurred!</h1>
+        <p>Failed to fetch meal data. Please try later.</p>
+    </div>
+  )
+}
