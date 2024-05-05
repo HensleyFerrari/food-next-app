2d72bd43a8e4ad4b98a1f3274092ea7f94694466
HensleyFerrari <hensley_ferrari@hotmail.com>
2024-05-05 10:55:29 -0300

    Added server-side input validation

---
+++ app/meals/share/error.jsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function MealsError() {
+  return (
+    <div className="error">
+        <h1>An error occurred!</h1>
+        <p>Failed to create Meal.</p>
+    </div>
+  )
+}
